9786dad0e50a8c2443127447e95f2f9b37977540
Alain Mauri <wildeng@hotmail.com>
2022-10-09 10:21:14 +0100

    Update readme refactor base class

--- lib/guardian_searcher/base.rb
+++ lib/guardian_searcher/base.rb
@@ -5,11 +5,8 @@ module GuardianSearcher
     include Faraday
 
     attr_reader :api_key
-    attr_accessor :base_uri
 
     def initialize(api_key: nil)
-      @base_uri = "https://content.guardianapis.com"
-
       raise GuardianApyKeyError unless api_key
 
       @api_key = api_key
@@ -17,19 +14,41 @@ module GuardianSearcher
 
     # Options needs to be passed following Guardian API docs
     def search(q, options = {})
-      opt = build_options(options)
-
-      url = @base_uri + "/search?q=#{q}&#{opt}&api-key=#{@api_key}"
+      url = search_uri + query_string(q, options)
       Faraday.get(url)
     end
 
     def search_sections(q, options = {})
-      opt = build_options(options)
-      url = @base_uri + "/sections?q=#{q}&#{opt}&api-key=#{@api_key}"
+      url = sections_uri + query_string(q, options)
       Faraday.get(url)
     end
 
     private
+
+    def base_uri
+      "https://content.guardianapis.com"
+    end
+
+    def sections_uri
+      "#{base_uri}/sections"
+    end
+
+    def search_uri
+      "#{base_uri}/search"
+    end
+
+    def tags_uri
+      "#{base_uri}/tags"
+    end
+
+    def editions_uri
+      "#{base_uri}/editions"
+    end
+
+    def query_string(q, options = {})
+      opt = build_options(options)
+      "?q=#{q}&#{opt}&api-key=#{@api_key}"
+    end
 
     def build_options(options)
       Options.new(options).build_options
