ff976fca4daeefbdc2aa5b2d6e0c2fc8fec4bcb9
Alain Mauri <wildeng@hotmail.com>
2024-12-21 17:46:14 +0000

    Improve test coverage

--- spec/guardian_searcher/search_result_spec.rb
+++ spec/guardian_searcher/search_result_spec.rb
@@ -30,6 +30,45 @@ RSpec.describe GuardianSearcher::SearchResult do
         end
       end
 
+      it "raises an exception for 500" do
+        VCR.use_cassette("returns_a_500") do
+          searcher = GuardianSearcher::Search.new(api_key: "test")
+          response = searcher.search("test", { "from_date": "okidoki" })
+          expect(response.status).to eq(500)
+
+          expect { GuardianSearcher::SearchResult.parse_with_codes(response: response) }.to raise_error(
+            GuardianSearcher::GuardianInternalServerError,
+            JSON.parse(response.body)["message"]
+          )
+        end
+      end
+
+      it "raises an exception for 400" do
+        VCR.use_cassette("returns_a_400") do
+          searcher = GuardianSearcher::Search.new(api_key: "test")
+          response = searcher.search("test", { "from_date": "okidoki" })
+          expect(response.status).to eq(400)
+
+          expect { GuardianSearcher::SearchResult.parse_with_codes(response: response) }.to raise_error(
+            GuardianSearcher::GuardianBadRequestError,
+            JSON.parse(response.body)["message"]
+          )
+        end
+      end
+
+      it "raises an exception for unknown" do
+        VCR.use_cassette("returns_a_407") do
+          searcher = GuardianSearcher::Search.new(api_key: "test")
+          response = searcher.search("test", { "from_date": "okidoki" })
+          expect(response.status).to eq(407)
+
+          expect { GuardianSearcher::SearchResult.parse_with_codes(response: response) }.to raise_error(
+            GuardianSearcher::GuardianUnknownError,
+            JSON.parse(response.body)["message"]
+          )
+        end
+      end
+
       it "raises an exception for 401" do
         VCR.use_cassette("returns_a_401") do
           searcher = GuardianSearcher::Search.new(api_key: "xxxtest")
